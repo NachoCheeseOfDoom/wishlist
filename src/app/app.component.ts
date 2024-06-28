74db8ac42bf258713e91e57a182b9acfbaff3aec
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-06-28 13:06:35 -0600

    stared working on an angular wish list, sort of a todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn to be cool.'),
+    new WishItem('Drink coffee.', true),
+    new WishItem('Find a job that pays well.'),
+  ];
   title = 'wishlist';
 }
